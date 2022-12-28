c07719c8bf298bf4249faf6a037c2ba16adbe647
Abdulloh <alvatin060901@gmail.com>
2022-12-28 12:16:35 +0700

    undangan tailwindcss

---
+++ .history/tailwind.config_20221228094947.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors : {
+      primary : "#14b8a6",
+      secondary : "#64748b",
+      dark : "#0f172a"
+    },
+    fontFamily : {
+      davis : ['Daniel Davis']
+    }
+  },
+  },
+  plugins: [],
+}
